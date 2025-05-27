7b6e6593a9e31f520499902f32f338766c1b05ab
Hannah Casier <hannah.casier@me.com>
2025-05-27 16:25:59 +0200

    changed text to text-sm

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -11,23 +11,23 @@ const Footer = () => {
     <footer className="flex justify-between items-center w-full">
       <div className="flex flex-col items-center gap-1">
         <LuClock />
-        <p>Timeline</p>
+        <p className="text-sm">Timeline</p>
       </div>
       <div className="flex flex-col items-center gap-1">
         <LuHouse />
-        <p>Discover</p>
+        <p className="text-sm">Discover</p>
       </div>
       <div className="flex flex-col items-center gap-1">
         <LuSquarePlus />
-        <p>Check-in</p>
+        <p className="text-sm">Check-in</p>
       </div>
       <div className="flex flex-col items-center gap-1">
         <LuUsers />
-        <p>Friends</p>
+        <p className="text-sm">Friends</p>
       </div>
       <div className="flex flex-col items-center gap-1">
         <LuUser />
-        <p>Profile</p>
+        <p className="text-sm">Profile</p>
       </div>
     </footer>
   );
