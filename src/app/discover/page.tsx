16b798f1fa2c54d029ef208262e3a1f79c54a647
Hannah Casier <hannah.casier@me.com>
2025-05-29 13:03:08 +0200

    changed mb

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,11 +1,21 @@
+import { LuMapPin } from "react-icons/lu";
+
 const DiscoverPage = () => {
   return (
-    <div className="flex flex-col items-center ">
-      <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold">Discover Concerts</h1>
-        <p className="text-lg text-gray-500">Find live music near you</p>
+    <>
+      <div className="flex flex-col items-center mb-12">
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl font-bold">Discover Concerts</h1>
+          <p className="text-lg text-gray-500">Find live music near you</p>
+        </div>
       </div>
-    </div>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row items-center gap-2">
+          <LuMapPin className="text-2xl text-accent-cyan" />
+          <p className="text-lg text-gray-500">Antwerp</p>
+        </div>
+      </div>
+    </>
   );
 };
 export default DiscoverPage;
