a7e7abf43c8967dc699d4cd89888f63562c26cbc
Hannah Casier <hannah.casier@me.com>
2025-05-29 12:56:20 +0200

    add title

---
+++ src/app/discover/page.tsx
@@ -0,0 +1,11 @@
+const DiscoverPage = () => {
+  return (
+    <div className="flex flex-col items-center ">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold">Discover Concerts</h1>
+        <p className="text-lg text-gray-500">Find live music near you</p>
+      </div>
+    </div>
+  );
+};
+export default DiscoverPage;
