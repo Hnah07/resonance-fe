9f24b7b83c89bb16ccee68480042b930702fd6d1
Hannah Casier <hannah.casier@me.com>
2025-06-16 10:56:03 +0200

    cleanup

--- src/components/SummaryStatCards.tsx
+++ src/components/SummaryStatCards.tsx
@@ -8,25 +8,7 @@ import {
   LuStar,
 } from "react-icons/lu";
 import { useEffect, useState } from "react";
-
-interface SummaryStats {
-  concerts_this_year: number;
-  total_concerts: number;
-  countries_visited: number;
-  countries_list: string[];
-  favorite_genre: {
-    genre: string;
-    count: number;
-  };
-  most_seen_artist: {
-    name: string;
-    count: number;
-  };
-  top_venue: {
-    name: string;
-    count: number;
-  };
-}
+import { SummaryStats } from "@/types/summary-stats";
 
 export function SummaryStatCards() {
   const [stats, setStats] = useState<SummaryStats | null>(null);
---
+++ src/types/summary-stats.ts
@@ -0,0 +1,18 @@
+export interface SummaryStats {
+  concerts_this_year: number;
+  total_concerts: number;
+  countries_visited: number;
+  countries_list: string[];
+  favorite_genre: {
+    genre: string;
+    count: number;
+  };
+  most_seen_artist: {
+    name: string;
+    count: number;
+  };
+  top_venue: {
+    name: string;
+    count: number;
+  };
+}
